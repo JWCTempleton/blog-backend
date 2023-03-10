282cc0d574d2229ed7467c6f82cc3608054eab09
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-09 17:29:20 -0800

    Add test to check which author has submitted the most posts (verified working)

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -7,7 +7,43 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikes = -Infinity;
+  let favoriteBlog;
+  blogs.map((blog) => {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes;
+      favoriteBlog = blog;
+    }
+  });
+  return favoriteBlog;
+};
+
+const mostBlogs = (blogs) => {
+  const result = blogs.reduce((acc, blog) => {
+    if (!acc[blog.author]) {
+      acc[blog.author] = 0;
+    }
+    acc[blog.author]++;
+    return acc;
+  }, {});
+  let highestNumber = -Infinity;
+  let authorResult;
+  for (let author in result) {
+    if (result[author] > highestNumber) {
+      highestNumber = result[author];
+      authorResult = {
+        author: author,
+        blogs: result[author],
+      };
+    }
+  }
+  return authorResult;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
 };
