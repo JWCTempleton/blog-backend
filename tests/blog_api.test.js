d0d4ea466f52e891183d55161bb60861df0dcb5b
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-11 16:42:41 -0800

    Add test to ensure that a post can be added and that a blog with no amount of likes specified will default to 0 likes when added to the DB (verified working)

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,6 +29,46 @@ test("all blogs are returned", async () => {
   expect(response.body).toHaveLength(helper.initialPosts.length);
 });
 
+test("a valid post can be added", async () => {
+  const newBlog = {
+    title: "API Test Blog",
+    author: "JWCTempleton",
+    url: "www.google.com",
+    likes: 13,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.postsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialPosts.length + 1);
+
+  const contents = blogsAtEnd.map((n) => n.title);
+
+  expect(contents).toContain("API Test Blog");
+});
+
+test("A post without number of likes specified defaults to 0", async () => {
+  const newBlog = {
+    title: "This should have 0 likes",
+    author: "Jamie Baldwin",
+    url: "www.google.com",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.postsInDb();
+  const contents = blogsAtEnd.map((n) => n.likes);
+  expect(contents).toContain(0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
